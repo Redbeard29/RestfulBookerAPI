e0381694e0ed76ae1ba67743a58ca41a43e778ce
Redbeard29 <ben@benshermanmusic.com>
2021-10-19 12:07:10 -0400

    initial commit practicing basic GET requests

---
+++ RestfulBooker.py
@@ -0,0 +1,21 @@
+import requests
+
+API = 'https://restful-booker.herokuapp.com/'
+booking_ids = requests.get(API + 'booking')
+booking_id_data = booking_ids.json()
+
+
+def get_booking_details(nums_list):
+    bookings = {}
+    for num in nums_list:
+        response = requests.get(API + 'booking/' + str(num))
+        #In case API is being actively written to and users are deleted, avoid unfound elements
+        if response.status_code != 404:
+            bookings[num] = response.json()
+
+    return bookings
+
+user_ids = [23, 25, 11, 10, 3, 24, 19, 28, 21, 7, 34, 30, 27, 20, 29, 15, 5, 14, 35, 22, 18, 31, 12, 9, 4, 33, 16, 36, 32, 1, 17, 26, 8, 13, 6, 2]
+
+print(get_booking_details(user_ids))
+
